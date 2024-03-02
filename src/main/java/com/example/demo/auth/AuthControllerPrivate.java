d2deb88b011ce9bb1ac376c0b8b5d7afb196a8fa
Jose Luis Alonso Cano <joseuji223@gmail.com>
2024-03-02 21:33:22 +0100

    Token solucionado y funcionando

--- src/main/java/com/example/demo/auth/AuthControllerPrivate.java
+++ src/main/java/com/example/demo/auth/AuthControllerPrivate.java
@@ -4,15 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class AuthControllerPrivate {
-    @RestController
-    @RequestMapping("/api/v1")
-    @RequiredArgsConstructor
-    public class AuthController {
+
         @PostMapping(value = "secure")
         public String welcome(){
             return "Welcome from secure endpoint";
-        }
+
     }
 }
--- src/main/java/com/example/demo/jwt/JwtService.java
+++ src/main/java/com/example/demo/jwt/JwtService.java
@@ -1,6 +1,7 @@
 package com.example.demo.jwt;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -13,6 +14,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -36,5 +38,31 @@ public class JwtService {
     private Key getKey(){
         byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaim(token,Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username=getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) &&!isTokenExpired(token));
+    }
+
+    private Claims getAllClaims(String token){
+    return Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    public <T> T getClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims=getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token){
+        return getClaim(token,Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token){
+        return getExpiration(token).before(new Date());
     }
 }
