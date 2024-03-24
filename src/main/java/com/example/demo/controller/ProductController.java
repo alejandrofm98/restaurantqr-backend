4d39e8889bff14dd1d04c67dcb597271335cc0b2
Jose Luis Alonso Cano <joseluji22@hotmail.com>
2024-03-24 01:08:56 +0100

    feat: Se añade insertar productos, updatearlos y eliminarlos (imagen incluida)

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,8 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Product;
 import com.example.demo.services.ProductService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -16,15 +22,43 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @PostMapping("products")
-    public Product addProduct(@RequestBody Product product) {
-        return productService.saveProduct(product);
+    //Insertar productos + foto
+    @PostMapping(value = "products", consumes = {"multipart/form-data"})
+    public Product uploadImage(@RequestParam("image") MultipartFile file, @RequestParam("name") String name, @RequestParam("description") String description, @RequestParam("price") String price, @RequestParam("category") Integer category, @RequestParam("status") Integer status, @RequestParam("idempresa") Integer idempresa) throws IOException {
+        return productService.uploadImage(file, name, description, price, category, status, idempresa);
     }
 
-    @PutMapping("/products/{id}")
-    public Product updateProduct(@PathVariable("id") Integer productId, @RequestBody Product updatedProduct) {
-        return productService.updateProduct(productId, updatedProduct);
+    //Updatear productos + foto
+    @PutMapping("products/{id}")
+    public ResponseEntity<Product> updateProductWithImage(
+            @PathVariable Integer id,
+            @RequestParam(value = "image", required = false) MultipartFile file,
+            @RequestParam String name,
+            @RequestParam String description,
+            @RequestParam String price,
+            @RequestParam Integer category,
+            @RequestParam Integer status,
+            @RequestParam Integer idempresa
+    ) {
+        try {
+            Product updatedProduct = productService.updateProductWithImage(id, file, name, description, price, category, status, idempresa);
+            return ResponseEntity.ok(updatedProduct);
+        } catch (IOException | EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
+
+    //Delete productos + foto
+    @DeleteMapping("products/{id}")
+    public ResponseEntity<Void> deleteProductAndImage(@PathVariable Integer id) {
+        try {
+            productService.deleteProductAndImage(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException | IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 
 
 }
--- src/main/java/com/example/demo/services/ProductService.java
+++ src/main/java/com/example/demo/services/ProductService.java
@@ -2,8 +2,16 @@ package com.example.demo.services;
 
 import com.example.demo.entity.Product;
 import com.example.demo.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -20,17 +28,95 @@ public class ProductService {
     }
 
 
-    public Product updateProduct(Integer productId, Product updatedProduct) {
-        Product existingProduct = productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("Product not found"));
+    public Product uploadImage(MultipartFile file, String name, String description, String price, Integer category, Integer status, Integer idempresa) throws IOException {
+        Product product = new Product();
+        product.setName(name);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setCategory(category);
+        product.setStatus(status);
+        product.setIdempresa(idempresa);
 
-        existingProduct.setName(updatedProduct.getName());
-        existingProduct.setDescription(updatedProduct.getDescription());
-        existingProduct.setPrice(updatedProduct.getPrice());
-        existingProduct.setCategory(updatedProduct.getCategory());
-        existingProduct.setStatus(updatedProduct.getStatus());
-        existingProduct.setImage(updatedProduct.getImage());
+        String fileName = file.getOriginalFilename();
+        String directory = "images";
+        String imagePath = directory + "\\" + fileName; // Ruta de la imagen
 
-        return productRepository.save(existingProduct);
+        // Verificar si la carpeta existe, si no, crearla
+        Path directoryPath = Paths.get(directory);
+        if (!Files.exists(directoryPath)) {
+            Files.createDirectories(directoryPath);
+        }
+
+        // Escribir el archivo en la carpeta
+        byte[] bytes = file.getBytes();
+        Path path = Paths.get(imagePath);
+        Files.write(path, bytes);
+
+        product.setImage(imagePath);
+
+        return productRepository.save(product);
     }
+
+
+
+    public Product updateProductWithImage(Integer id, MultipartFile file, String name, String description, String price, Integer category, Integer status, Integer idempresa) throws IOException {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(name);
+            product.setDescription(description);
+            product.setPrice(price);
+            product.setCategory(category);
+            product.setStatus(status);
+            product.setIdempresa(idempresa);
+
+            if (file != null && !file.isEmpty()) {
+                String fileName = file.getOriginalFilename();
+                String directory = "images";
+                String imagePath = directory + "\\" + fileName; // Ruta de la imagen
+
+                // Verificar si la carpeta existe, si no, crearla
+                Path directoryPath = Paths.get(directory);
+                if (!Files.exists(directoryPath)) {
+                    Files.createDirectories(directoryPath);
+                }
+
+                // Escribir el archivo en la carpeta
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(imagePath);
+                Files.write(path, bytes);
+
+                product.setImage(imagePath);
+            }
+
+            return productRepository.save(product);
+        } else {
+            throw new EntityNotFoundException("Product with id " + id + " not found");
+        }
+    }
+
+
+
+
+        public void deleteProductAndImage(Integer id) throws IOException {
+            // Buscar el producto por ID
+            Product product = productRepository.findById(id)
+                    .orElseThrow(() -> new EntityNotFoundException("Product not found with id: " + id));
+
+            // Eliminar la imagen del disco
+            String imagePath = product.getImage();
+            if (imagePath != null) {
+                Path path = Paths.get(imagePath);
+                Files.deleteIfExists(path);
+            }
+
+            // Eliminar el producto de la base de datos
+            productRepository.deleteById(id);
+        }
+
+
+
+
+
+
 }
