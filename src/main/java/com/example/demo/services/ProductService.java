42e74321c1550f4050e63a28cab9880ef793e25c
Jose Luis Alonso Cano <joseluji22@hotmail.com>
2024-03-25 00:22:07 +0100

    feat: Se añade comprobacion de formato y cambio de nombre de la imagen por el del producto

--- src/main/java/com/example/demo/services/ProductService.java
+++ src/main/java/com/example/demo/services/ProductService.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Optional;
 
 @Service
@@ -37,32 +38,40 @@ public class ProductService {
         product.setStatus(status);
         product.setIdempresa(idempresa);
 
-        String fileName = file.getOriginalFilename();
-        String directory = "images";
-        String imagePath = directory + "\\" + fileName; // Ruta de la imagen
+        String originalFileName = file.getOriginalFilename();
+        String fileExtension = getFileExtension(originalFileName);
+        if (fileExtension != null && (fileExtension.equalsIgnoreCase("jpg") || fileExtension.equalsIgnoreCase("jpeg"))) {
+            String newFileName = name.replaceAll("\\s+", "_") + "." + fileExtension; // Nombre del producto con espacios reemplazados por guiones bajos y extensión original
+            String directory = "images";
+            String imagePath = directory + "\\" + newFileName; // Ruta de la imagen
 
-        // Verificar si la carpeta existe, si no, crearla
-        Path directoryPath = Paths.get(directory);
-        if (!Files.exists(directoryPath)) {
-            Files.createDirectories(directoryPath);
+            // Verificar si la carpeta existe, si no, crearla
+            Path directoryPath = Paths.get(directory);
+            if (!Files.exists(directoryPath)) {
+                Files.createDirectories(directoryPath);
+            }
+
+            // Escribir el archivo en la carpeta
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(imagePath);
+            Files.write(path, bytes);
+
+            product.setImage(imagePath);
+        } else {
+            throw new IllegalArgumentException("The file must be of type JPG or JPEG.");
         }
-
-        // Escribir el archivo en la carpeta
-        byte[] bytes = file.getBytes();
-        Path path = Paths.get(imagePath);
-        Files.write(path, bytes);
-
-        product.setImage(imagePath);
 
         return productRepository.save(product);
     }
-
 
 
     public Product updateProductWithImage(Integer id, MultipartFile file, String name, String description, String price, Integer category, Integer status, Integer idempresa) throws IOException {
         Optional<Product> optionalProduct = productRepository.findById(id);
         if (optionalProduct.isPresent()) {
             Product product = optionalProduct.get();
+            String oldImage = product.getImage(); // Obtener la imagen actual del producto
+            String newFileName = name.replaceAll("\\s+", "_") + "." + getFileExtension(oldImage); // Nuevo nombre de la imagen
+
             product.setName(name);
             product.setDescription(description);
             product.setPrice(price);
@@ -71,27 +80,54 @@ public class ProductService {
             product.setIdempresa(idempresa);
 
             if (file != null && !file.isEmpty()) {
-                String fileName = file.getOriginalFilename();
-                String directory = "images";
-                String imagePath = directory + "\\" + fileName; // Ruta de la imagen
+                String originalFileName = file.getOriginalFilename();
+                String extension = getFileExtension(originalFileName);
+                if (extension != null && (extension.equalsIgnoreCase("jpg") || extension.equalsIgnoreCase("jpeg"))) {
+                    String directory = "images";
+                    String imagePath = directory + "\\" + newFileName; // Ruta de la nueva imagen
 
-                // Verificar si la carpeta existe, si no, crearla
-                Path directoryPath = Paths.get(directory);
-                if (!Files.exists(directoryPath)) {
-                    Files.createDirectories(directoryPath);
+                    // Verificar si la carpeta existe, si no, crearla
+                    Path directoryPath = Paths.get(directory);
+                    if (!Files.exists(directoryPath)) {
+                        Files.createDirectories(directoryPath);
+                    }
+
+                    // Escribir el archivo en la carpeta
+                    byte[] bytes = file.getBytes();
+                    Path path = Paths.get(imagePath);
+                    Files.write(path, bytes);
+
+                    product.setImage(imagePath);
+
+                    // Eliminar la imagen anterior si existe
+                    if (oldImage != null && !oldImage.equals(imagePath)) {
+                        Files.deleteIfExists(Paths.get(oldImage));
+                    }
+                } else {
+                    throw new IllegalArgumentException("The file must be of type JPG or JPEG.");
                 }
-
-                // Escribir el archivo en la carpeta
-                byte[] bytes = file.getBytes();
-                Path path = Paths.get(imagePath);
-                Files.write(path, bytes);
-
-                product.setImage(imagePath);
+            } else {
+                // Renombrar la imagen asociada si el nombre del producto cambia
+                if (!oldImage.equals(newFileName)) {
+                    Path oldPath = Paths.get(oldImage);
+                    Path newPath = Paths.get("images" + "\\" + newFileName);
+                    Files.move(oldPath, newPath, StandardCopyOption.REPLACE_EXISTING);
+                    product.setImage(newPath.toString());
+                }
             }
 
             return productRepository.save(product);
         } else {
             throw new EntityNotFoundException("Product with id " + id + " not found");
+        }
+    }
+
+
+    private String getFileExtension(String fileName) {
+        if (fileName != null && fileName.lastIndexOf(".") != -1) {
+            return fileName.substring(fileName.lastIndexOf(".") + 1);
+        } else {
+            return null;
         }
     }
 
