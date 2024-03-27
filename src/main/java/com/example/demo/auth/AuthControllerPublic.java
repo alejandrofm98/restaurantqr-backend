32506a57fce3c639d43258fea2363b497667d6b2
Jose Luis Alonso Cano <joseluji22@hotmail.com>
2024-03-27 14:15:53 +0100

    feat: Se añade el poder listar los productos desde una ruta desprotegida

--- src/main/java/com/example/demo/auth/AuthControllerPublic.java
+++ src/main/java/com/example/demo/auth/AuthControllerPublic.java
@@ -2,14 +2,15 @@ package com.example.demo.auth;
 
 
 import com.example.demo.config.Log4j2Config;
+import com.example.demo.entity.Product;
 import com.example.demo.request.LoginRequest;
 import com.example.demo.request.RegisterRequest;
+import com.example.demo.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.example.demo.utils.Constants.*;
 
@@ -20,6 +21,7 @@ import static com.example.demo.utils.Constants.*;
 public class AuthControllerPublic {
 
     private final AuthService authService;
+    private final ProductService productService;
 
     @PostMapping(value ="login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
@@ -40,6 +42,15 @@ public class AuthControllerPublic {
                     request.toString()
             );
             return ResponseEntity.ok(authResponse);
+    }
+
+    @GetMapping("products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        Log4j2Config.logRequestInfo(CONSTANT_GET, CONSTANT_LOGIN_URL + "/products",
+                "All products displayed correctly",
+                products.toString());
+        return ResponseEntity.ok(products);
     }
 
 }
