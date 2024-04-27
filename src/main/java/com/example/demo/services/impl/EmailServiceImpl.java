7f51bcf004df723e6ebdc79ccd784d95b93bbc0b
alejandro <alejandrofmayor98@gmail.com>
2024-04-27 22:43:22 +0200

    fix: arreglado envio correos con adjunto y html

--- src/main/java/com/example/demo/services/impl/EmailServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/EmailServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.demo.config.Log4j2Config;
 import com.example.demo.dto.EmailDetails;
 import com.example.demo.exception.Exceptions;
 import com.example.demo.services.EmailService;
+import jakarta.mail.Message.RecipientType;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import java.io.File;
@@ -11,7 +12,6 @@ import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -34,7 +34,7 @@ public class EmailServiceImpl implements EmailService {
 
       // Setting up necessary details
       message.setFrom(sender);
-      message.setRecipients(MimeMessage.RecipientType.TO,details.getRecipient());
+      message.setRecipients(RecipientType.TO,details.getRecipient());
       message.setContent(details.getMsgBody(),"text/html; charset=utf-8");
       message.setSubject(details.getSubject());
 
@@ -42,7 +42,7 @@ public class EmailServiceImpl implements EmailService {
       // Sending the mail
       javaMailSender.send(message);
       Log4j2Config.logMethod("sendMail()",
-          "Mail Sent Succesfully from " + sender + " to " + details.getRecipient()
+          "Mail Sent Successfully from " + sender + " to " + details.getRecipient()
               + " with Subject: " + details.getSubject());
     }
 
@@ -70,7 +70,7 @@ public class EmailServiceImpl implements EmailService {
           = new MimeMessageHelper(mimeMessage, true);
       mimeMessageHelper.setFrom(sender);
       mimeMessageHelper.setTo(details.getRecipient());
-      mimeMessageHelper.setText(details.getMsgBody());
+      mimeMessageHelper.setText(details.getMsgBody(),true);
       mimeMessageHelper.setSubject(
           details.getSubject());
 
@@ -95,7 +95,7 @@ public class EmailServiceImpl implements EmailService {
     catch (MessagingException e) {
       String message = "Failure sending mail from " + sender + " to " + details.getRecipient()
           + " with Subject: " + details.getSubject();
-      Log4j2Config.logMethod("sendMail()", message, e.getMessage());
+      Log4j2Config.logMethod("sendMailWithAttachment()", message, e.getMessage());
       throw new Exceptions(message);
     }
   }
