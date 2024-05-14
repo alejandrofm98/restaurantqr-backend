364fa04130d7bcaf0373ad1e551e59532ccedde2
alejandro <alejandrofmayor98@gmail.com>
2024-05-14 18:41:59 +0200

    fix: arreglada envio de fechas y refactorizado codigo.

--- src/main/java/com/example/demo/services/OrderService.java
+++ src/main/java/com/example/demo/services/OrderService.java
@@ -2,12 +2,15 @@ package com.example.demo.services;
 
 import com.example.demo.dto.OrderLineRequest;
 import com.example.demo.dto.OrderRequest;
+import com.example.demo.entity.Business;
 import com.example.demo.entity.Order;
 import com.example.demo.entity.OrderLine;
 import com.example.demo.repository.BusinessRepository;
 import com.example.demo.repository.OrderLineRepository;
 import com.example.demo.repository.OrderRepository;
 import com.example.demo.repository.ProductRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -23,14 +26,29 @@ public class OrderService {
   private final ProductRepository productRepository;
   private final BusinessRepository businessRepository;
 
+
   public Order getOrder(Long id) {
     Optional<Order> order = orderRepository.findById(id);
-    return order.orElseThrow(() -> new RuntimeException("Order not found"));
+    return order.orElseThrow(() -> new EntityNotFoundException("Order not found"));
+  }
+
+  public List<Order> getOrders() {
+    List<Order> orders = orderRepository.findAll();
+    if (orders.isEmpty()) {
+      throw new EntityNotFoundException("Orders not found");
+    }
+    return orders;
+  }
+
+  public Order saveOrder(Order order) {
+    return orderRepository.save(order);
   }
 
   public Order createOrder(OrderRequest orderRequest) {
     Order order = new Order();
-    order.setBusiness(businessRepository.findById(orderRequest.getBusinessUuid()).get());
+    Business business = businessRepository.findById(orderRequest.getBusinessUuid()).orElseThrow(
+        () -> new EntityNotFoundException("Business not found"));
+    order.setBusiness(business);
     order.setOrderNumber(this.calculateNextOrderId(orderRequest.getBusinessUuid()));
     Order resultado = orderRepository.save(order);
     resultado.setOrderLines(createOrderLine(orderRequest, resultado.getId()));
@@ -48,9 +66,9 @@ public class OrderService {
     List<OrderLine> orderLines = new ArrayList<>();
     for (OrderLineRequest o : orderRequest.getOrderLines()) {
       OrderLine orderLine = new OrderLine();
-      Order order = orderRepository.findById(orderId).get();
-      orderLine.setOrder(order);
-      orderLine.setProduct(productRepository.findById(o.getProductId()).get());
+      orderLine.setOrder(getOrder(orderId));
+      orderLine.setProduct(productRepository.findById(o.getProductId())
+          .orElseThrow(() -> new EntityNotFoundException("Product not found")));
       orderLine.setLineNumber(calculateNextLineNumber(orderId));
       orderLine.setQuantity(o.getQuantity());
       orderLine.setObservations(o.getObservations());
@@ -66,7 +84,12 @@ public class OrderService {
 
   }
 
-  public void deleteOrder(Long id){
-     orderRepository.deleteById(id);
+  public void deleteOrder(Long id) {
+    orderRepository.deleteById(id);
+    if (orderRepository.existsById(id)) {
+      throw new EntityExistsException("Order not deleted.");
+    }
+
+
   }
 }
