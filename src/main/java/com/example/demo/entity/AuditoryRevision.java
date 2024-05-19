c6d2b04096be4ada5824151e0fac198461930730
alejandro <alejandrofmayor98@gmail.com>
2024-05-19 19:00:27 +0200

    feat: nuevo catalogo para la auditoria
    Se crea un nuevo "catalogo" o base de datos exclusivo para guardar los datos de la auditoria, tambien se actualiza el application-example.properties para poder utilizarlo.

--- src/main/java/com/example/demo/entity/AuditoryRevision.java
+++ src/main/java/com/example/demo/entity/AuditoryRevision.java
@@ -3,12 +3,16 @@ package com.example.demo.entity;
 import com.example.demo.config.MyRevisionListener;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
+@Table(name = "auditory_revision", catalog = "auditory")
 @RevisionEntity(MyRevisionListener.class)
 public class AuditoryRevision extends DefaultRevisionEntity {
   @Id
