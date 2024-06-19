9b790a8ed7abb3a523fdfd2cda176d0d7ad593cd
alejandro <alejandrofmayor98@gmail.com>
2024-06-19 20:53:06 +0200

    refactor: Se modifica el código para devolver objeto ApiResponse desde NotificationController.

--- src/main/java/com/example/demo/firebase/NotificationController.java
+++ src/main/java/com/example/demo/firebase/NotificationController.java
@@ -1,7 +1,11 @@
 package com.example.demo.firebase;
 
+import com.example.demo.dto.ApiResponse;
 import com.google.firebase.messaging.FirebaseMessagingException;
+import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 import static com.example.demo.utils.Constants.CONSTANT_PUBLIC_URL;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(CONSTANT_PUBLIC_URL)
 public class NotificationController {
-    @Autowired
-    FirebaseMessagingService firebaseMessagingService;
+
+    private final FirebaseMessagingService firebaseMessagingService;
 
     @PostMapping(value = "notification")
-    public String sendNotificationByToken(@RequestBody NotificationMessage notificationMessage) throws FirebaseMessagingException {
-        return firebaseMessagingService.sendNotificationByToken(notificationMessage);
+    public ResponseEntity<ApiResponse> sendNotificationByToken(@RequestBody NotificationMessage notificationMessage) throws FirebaseMessagingException {
+        ApiResponse apiResponse = ApiResponse.builder()
+            .response(firebaseMessagingService.sendNotificationByToken(notificationMessage))
+            .build();
+        return ResponseEntity.ok(apiResponse);
     }
 }
