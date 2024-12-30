a758cc76173f4cd485db2456d4fb8f70bfbf4736
alejandrofm98 <alejandrofmayor98@gmail.com>
2024-12-30 01:03:38 +0100

    feat: Se modifica el datasource

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -5,16 +5,18 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import jakarta.annotation.PostConstruct;
+import java.sql.SQLException;
 import javax.sql.DataSource;
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Log4j2
 @Profile("dev|pro")
+@Log4j2
 @Configuration
 public class DataSourceConfig {
 
@@ -45,6 +47,8 @@ public class DataSourceConfig {
   @Value("${database.port}")
   private int databasePort;
 
+  @Value("${spring.datasource.url.docker}")
+  private String dockerUrl;
 
   @PostConstruct
   public void setupSSHTunnel() {
@@ -58,17 +62,28 @@ public class DataSourceConfig {
       session.setPortForwardingL(databasePort, sshHost, databasePort);
 
     } catch (JSchException e) {
-       log.info("Fallo al crear el ssh tunnel para conectar a la bd");
+      log.info("Fallo al crear el ssh tunnel para conectar a la bd");
     }
   }
 
   @Bean
   public DataSource getDataSource() {
+    return getDB(driverClassName, url, username, password, dockerUrl);
+  }
+
+  static DataSource getDB(String driverClassName, String url, String username,
+      String password, String dockerUrl) {
     DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
     dataSourceBuilder.driverClassName(driverClassName);
     dataSourceBuilder.url(url);
     dataSourceBuilder.username(username);
     dataSourceBuilder.password(password);
+    try {
+      dataSourceBuilder.build().getConnection();
+    } catch (SQLException e) {
+      log.error(e);
+      dataSourceBuilder.url(dockerUrl);
+    }
     return dataSourceBuilder.build();
   }
 }
